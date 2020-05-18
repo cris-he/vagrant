28e241b6aa5118bec3817ae37b7d8c9f5023fe29
cris <cris@Criss-MacBook-Pro.local>
2020-05-18 00:05:01 -0400

    nice

--- scripts/vault/init.sh
+++ scripts/vault/init.sh
@@ -2,17 +2,14 @@
 # echo "source /vagrant/configs/vault/.bashrc" >> /home/vagrant/.bashrc
 echo "export VAULT_ADDR=http://127.0.0.1:8200" >> /home/vagrant/.bashrc
 
-echo "CONSULE_ADDR" ${CONSUL_ADDR}
-
 /usr/local/bin/vault operator init | tee /tmp/vault.init > /dev/null
 
-# Store master keys in consul for operator to retrieve and remove
 COUNTER=1
 cat /tmp/vault.init | grep '^Unseal' | awk '{print $4}' | for key in $(cat -); do
-curl -fX PUT 192.168.33.10:8500/v1/kv/vault/secrets/unseal-key-$COUNTER -d $key
+curl -fX PUT "$CONSUL_ADDR/v1/kv/vault/secrets/unseal-key-$COUNTER" -d $key
 COUNTER=$((COUNTER + 1))
 done
 
 export ROOT_TOKEN=$(cat /tmp/vault.init | grep '^Initial' | awk '{print $4}')
 echo "export VAULT_TOKEN=${ROOT_TOKEN}" >> /home/vagrant/.bashrc
-curl -fX PUT 192.168.33.10:8500/v1/kv/vault/secrets/root-token -d $ROOT_TOKEN
+curl -fX PUT "$CONSUL_ADDR/v1/kv/vault/secrets/root-token" -d $ROOT_TOKEN
--- scripts/vault/unseal.sh
+++ scripts/vault/unseal.sh
@@ -1,8 +1,6 @@
-echo "CONSULE_ADDR" ${CONSUL_ADDR}
-
-cget() { curl -sf "http://192.168.33.10:8500/v1/kv/vault/secrets/$1?raw"; }
+cget() { curl -sf "http://$CONSUL_ADDR/v1/kv/vault/secrets/$1?raw"; }
 
 echo "Unsealing Vault"
-/usr/local/bin/vault operator unseal $(cget unseal-key-1)
-/usr/local/bin/vault operator unseal $(cget unseal-key-2)
-/usr/local/bin/vault operator unseal $(cget unseal-key-3)
+/usr/local/bin/vault operator unseal $(cget unseal-key-1) &> /dev/null
+/usr/local/bin/vault operator unseal $(cget unseal-key-2) &> /dev/null
+/usr/local/bin/vault operator unseal $(cget unseal-key-3) &> /dev/null
