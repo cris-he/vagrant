c1e2e04497ad9a057b7b41d85f90507af30f30e1
cris <cris@Criss-MacBook-Pro.local>
2020-09-21 04:27:37 -0400

    configs & scripts

---
+++ scripts/basics.sh
@@ -0,0 +1,8 @@
+# Basics
+sudo yum install -y -q wget
+sudo yum install -y -q unzip
+sudo yum install -y -q vim
+
+# Node.js
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install -y nodejs
